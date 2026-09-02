cce8e5ae44a10853d0ac6439ee6ae66fd986dc16
lucia.rincon.contractor@opplus.bbva.com <lucia.rincon.contractor@opplus.bbva.com>
2026-09-02 13:47:56 +0200

    quito puntos ruptura

--- pages/AnalyticsDashboardPage.ts
+++ pages/AnalyticsDashboardPage.ts
@@ -50,7 +50,6 @@ export class AnalyticsDashboardPage extends BasePage {
      * @returns Total item count reported by the table pager.
      */
     async getDataTableItemCount(tableTitle: string): Promise<number> {
-        // await this._page.pause();
         await this.waitImplicit(5000);
         return this.getKendoPagerItemCount(this.dataTablePagerInfoByTitle(tableTitle));
     }
