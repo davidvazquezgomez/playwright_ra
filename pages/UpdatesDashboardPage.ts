0dfa9f21da8430f6a0069c9fa34d467fceabc789
evazquezvazquez <evazquezvazquez@deloitte.es>
2026-09-09 14:25:59 +0200

    pause removal

--- pages/UpdatesDashboardPage.ts
+++ pages/UpdatesDashboardPage.ts
@@ -517,7 +517,6 @@ export class UpdatesDashboardPage extends BasePage {
     userName: string,
     fieldName: 'User Assigned' | 'Watch List',
   ): Promise<void> {
-    // await this._page.pause();
     const peoplePicker = this.updateDetailsPeoplePickerByField(fieldName);
     const searchInputSelector = this.updateDetailsPeoplePickerSearchInputByFieldName[fieldName];
     const userOptionSelector = this.updateDetailsPeoplePickerOptionByName(userName);
